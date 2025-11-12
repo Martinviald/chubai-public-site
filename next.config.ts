ee639c3bc51aaae89ca02948b7cd0cd87a537f4b
Martin Vial <martinviald@uc.cl>
2025-11-11 23:39:18 -0300

    Arreglo de env

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    // Variables del sistema de email
+    EMAIL_SERVICE_HOST: process.env.EMAIL_SERVICE_HOST,
+    EMAIL_SERVICE_PORT: process.env.EMAIL_SERVICE_PORT,
+    EMAIL_SERVICE_SECURE: process.env.EMAIL_SERVICE_SECURE,
+    EMAIL_SERVICE_USER: process.env.EMAIL_SERVICE_USER,
+    EMAIL_SERVICE_PASS: process.env.EMAIL_SERVICE_PASS,
+    EMAIL_DESTINATION_USER: process.env.EMAIL_DESTINATION_USER,
+    
+    // Variables públicas (si las hay)
+    // NEXT_PUBLIC_: process.env.NEXT_PUBLIC_,
+  }
 };
 
 export default nextConfig;
